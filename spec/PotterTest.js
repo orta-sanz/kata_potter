6071628855063b6c006e72b974a463bf94909be7
orta-sanz <orta.sanz.93@gmail.com>
2014-04-10 13:40:31 +0200

    Función Mete libro falla

--- spec/PotterTest.js
+++ spec/PotterTest.js
@@ -6,4 +6,10 @@ describe('Carrito', function() {
 	it('La cantidad de libros de Potter #1 en el carro debe ser 0', function() {
 		expect(devuelveCantidadLibro(1)).toBe(0);
 	});
+
+	it('Añadimos dos libros de Potter #4 al carro', function() {
+		meteLibro(1, 2);
+
+		expect(devuelveCantidadLibro(1)).toBe(2);
+	});
 });
