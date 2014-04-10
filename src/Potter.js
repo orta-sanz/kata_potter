91733a223d409e04e2b8c5360e51bcb6970a0248
orta-sanz <orta.sanz.93@gmail.com>
2014-04-10 08:50:15 +0200

    Commit inicial, ficheros y directorios

---
+++ src/Potter.js
@@ -0,0 +1,14 @@
+var articulos 	= {potter_1: 0, }
+var carro 		= {cantidad: 0, articulos: [], total_euros: 0};
+
+function devuelveCantidad() {
+	return carro.cantidad;
+}
+
+function devuelveArticulos() {
+	return carro.articulos.length;
+}
+
+function devuelveTotal() {
+	return carro.total_euros;
+}
