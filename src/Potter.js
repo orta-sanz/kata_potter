7431ac8fd8d4efc28ce0c93e5ff1b01be47f8ea7
orta-sanz <orta.sanz.93@gmail.com>
2014-04-10 08:57:26 +0200

    El carrito se inicia con 0 articulos y 0 euros totales

--- src/Potter.js
+++ src/Potter.js
@@ -1,13 +1,9 @@
-var articulos 	= {potter_1: 0, }
-var carro 		= {cantidad: 0, articulos: [], total_euros: 0};
+var carro 		= {cantidad: 0, total_euros: 0};
 
 function devuelveCantidad() {
 	return carro.cantidad;
 }
 
-function devuelveArticulos() {
-	return carro.articulos.length;
-}
 
 function devuelveTotal() {
 	return carro.total_euros;
